4e0f468822018f759b5fd90f9788e1dab5d7eb57
Nathan Jones <thenathanjones@gmail.com>
2012-02-28 21:15:11 +0800

    Initial scaffold commit

---
+++ Specs/Pages/BasePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Specs.Pages
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver Browser { get; set; }
+
+        protected string Url { get; set; }
+
+        protected BasePage(IWebDriver browser)
+        {
+            Browser = browser;
+        }
+
+        public void Navigate()
+        {
+            if (String.IsNullOrEmpty(Url))
+            {
+                throw new ArgumentNullException("Url", "Pages must set their Url");
+            }
+
+            Browser.Navigate().GoToUrl(Url);
+        }
+    }
+}
